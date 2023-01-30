de5c3ac29c32742eacf073db24e28127803bc9cd
abilan <abilan@vmware.com>
2023-01-30 14:16:40 -0500

    Fix test for KCL

--- kinesis-binder-observation-demo/src/test/java/com/example/kinesisbinderobservationdemo/KinesisBinderObservationDemoApplicationTests.java
+++ kinesis-binder-observation-demo/src/test/java/com/example/kinesisbinderobservationdemo/KinesisBinderObservationDemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.kinesisbinderobservationdemo;
 
+import java.net.URI;
 import java.util.Map;
 
 import com.amazonaws.services.kinesis.AmazonKinesisAsync;
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream;
+import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
@@ -12,8 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.stream.binder.EmbeddedHeaderUtils;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.aws.inbound.kinesis.KinesisMessageDrivenChannelAdapter;
-import org.springframework.integration.aws.inbound.kinesis.KinesisShardOffset;
+import org.springframework.integration.aws.inbound.kinesis.KclMessageDrivenChannelAdapter;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.messaging.Message;
 import org.springframework.test.annotation.DirtiesContext;
@@ -58,6 +61,23 @@ class KinesisBinderObservationDemoApplicationTests implements LocalstackContaine
 		}
 
 		@Bean
+		public KinesisProducerConfiguration kinesisProducerConfiguration() {
+			URI kinesisUri =
+					LocalstackContainerTest.LOCAL_STACK_CONTAINER.getEndpointOverride(LocalStackContainer.Service.KINESIS);
+			URI cloudWatchUri =
+					LocalstackContainerTest.LOCAL_STACK_CONTAINER.getEndpointOverride(LocalStackContainer.Service.CLOUDWATCH);
+
+			return new KinesisProducerConfiguration()
+					.setCredentialsProvider(LocalstackContainerTest.credentialsProvider())
+					.setRegion(LocalstackContainerTest.LOCAL_STACK_CONTAINER.getRegion())
+					.setKinesisEndpoint(kinesisUri.getHost())
+					.setKinesisPort(kinesisUri.getPort())
+					.setCloudwatchEndpoint(cloudWatchUri.getHost())
+					.setCloudwatchPort(cloudWatchUri.getPort())
+					.setVerifyCertificate(false);
+		}
+
+/*		@Bean
 		public KinesisMessageDrivenChannelAdapter kinesisInboundChannelChannel(
 				AmazonKinesisAsync amazonKinesis,
 				QueueChannel kinesisReceiveChannel) {
@@ -65,6 +85,26 @@ class KinesisBinderObservationDemoApplicationTests implements LocalstackContaine
 			KinesisMessageDrivenChannelAdapter adapter =
 					new KinesisMessageDrivenChannelAdapter(amazonKinesis, "my-event");
 			adapter.setStreamInitialSequence(KinesisShardOffset.trimHorizon());
+			adapter.setOutputChannel(kinesisReceiveChannel);
+			adapter.setEmbeddedHeadersMapper(Configuration::unembedHeaders);
+			adapter.setConverter(String::new);
+			return adapter;
+		}*/
+
+		@Bean
+		public KclMessageDrivenChannelAdapter kclMessageDrivenChannelAdapter(
+				AmazonKinesisAsync amazonKinesis,
+				QueueChannel kinesisReceiveChannel) {
+
+			KclMessageDrivenChannelAdapter adapter =
+					new KclMessageDrivenChannelAdapter(
+							"my-event",
+							amazonKinesis,
+							LocalstackContainerTest.cloudWatchClient(),
+							LocalstackContainerTest.dynamoDbClient(),
+							LocalstackContainerTest.credentialsProvider());
+			adapter.setOutputChannel(kinesisReceiveChannel());
+			adapter.setStreamInitialSequence(InitialPositionInStream.TRIM_HORIZON);
 			adapter.setOutputChannel(kinesisReceiveChannel);
 			adapter.setEmbeddedHeadersMapper(Configuration::unembedHeaders);
 			adapter.setConverter(String::new);
