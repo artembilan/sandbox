6a2ea5646eaa2294a5404b4abc8658d9b9024571
Artem Bilan <abilan@vmware.com>
2023-09-15 15:07:12 -0400

    Propagate security context via message channels

---
+++ spring-integration-security-context-propagation/src/test/java/org/springframework/integration/security/sample/springintegrationsecuritycontextpropagation/SpringIntegrationSecurityContextPropagationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.integration.security.sample.springintegrationsecuritycontextpropagation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationSecurityContextPropagationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
